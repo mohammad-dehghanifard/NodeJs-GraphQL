4b17062ce1783f9b1f423b71ad7feb8bafeb7c6c
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-02 01:36:26 -0800

    impl online shop schema

--- app.js
+++ app.js
@@ -37,10 +37,47 @@ type OrderItem {
 `);
 
 var shoapRoot = {
-   // TODO: implement test data
+    products : [
+    {
+        id: "Hat1",
+        title: "Hat 1 product",
+        description : "this product is hat 1",
+        price : 127.63
+    },
+    {
+        id: "Hat2",
+        title: "Hat 2 product",
+        description : "this product is hat 2",
+        price : 45.99
+    },
+    {
+        id: "Hat3",
+        title: "Hat 3 product",
+        description : "this product is hat 3",
+        price : 12.39
+    }
+  ],
+  orders : [
+    
+    {
+        date : '2022-06-18',
+        totalPrice : 236.84,
+        items : [
+            {
+                product : {
+                    id: "Hat2",
+                    title: "Hat 2 product",
+                    description : "this product is hat 2",
+                    price : 45.99
+                },
+                quantity: 6
+            }
+        ]
+    }
+  ]
 }
 
-app.use("/graphql",createHandler({schema: schema,rootValue : graphRoot}));
+app.use("/graphql",createHandler({schema: schema,rootValue : shoapRoot}));
 app.get('/playground',expressPlayground({endpoint : '/graphql'}));
 
 app.listen(3000, () => {
