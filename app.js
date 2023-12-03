4a5ace970764aab53265bbd697b894f64033876b
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-03 00:33:31 -0800

    add product fake data

--- app.js
+++ app.js
@@ -2,60 +2,18 @@ const express = require('express');
 const { buildSchema } = require('graphql');
 const {createHandler} = require('graphql-http/lib/use/express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
+const path = require('path');
+const {loadFilesSync} = require("@graphql-tools/load-files");
+const {makeExecutableSchema} = require("@graphql-tools/schema");
 
-const app = express()
 
-const schema = buildSchema(`
-type Query {
-    products : [Product]
-    orders : [Order]
-}
+const schemaArray = loadFilesSync(path.join(__dirname,"**/*.graphql"));
 
-type Product {
-    id : ID!
-    title : String
-    description : String
-    price : Float
-    review : [Review]
-}
-
-type Review {
-    rating : Int!
-    comment : String
-}
-
-type Order {
-    date : String!
-    totalPrice : Float
-    items : [OrderItem]
-}
-
-type OrderItem {
-    product : Product!
-    quantity : Int!
-}
-`);
+const schema = makeExecutableSchema({typeDefs: schemaArray});
 
 var shoapRoot = {
     products : [
-    {
-        id: "Hat1",
-        title: "Hat 1 product",
-        description : "this product is hat 1",
-        price : 127.63
-    },
-    {
-        id: "Hat2",
-        title: "Hat 2 product",
-        description : "this product is hat 2",
-        price : 45.99
-    },
-    {
-        id: "Hat3",
-        title: "Hat 3 product",
-        description : "this product is hat 3",
-        price : 12.39
-    }
+  
   ],
   orders : [
     
@@ -76,7 +34,7 @@ var shoapRoot = {
     }
   ]
 }
-
+const app = express();
 app.use("/graphql",createHandler({schema: schema,rootValue : shoapRoot}));
 app.get('/playground',expressPlayground({endpoint : '/graphql'}));
 
---
+++ data/products.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "Hat1",
+        title: "Hat 1 product",
+        description : "this product is hat 1",
+        price : 127.63
+    },
+    {
+        id: "Hat2",
+        title: "Hat 2 product",
+        description : "this product is hat 2",
+        price : 45.99
+    },
+    {
+        id: "Hat3",
+        title: "Hat 3 product",
+        description : "this product is hat 3",
+        price : 12.39
+    }
+]
