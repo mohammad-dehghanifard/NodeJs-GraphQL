821f34d04ac900f4bf26e547c6da56289a02159f
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-12-03 00:38:02 -0800

    impl order fake data and update project

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const { buildSchema } = require('graphql');
 const {createHandler} = require('graphql-http/lib/use/express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
 const path = require('path');
+const productList = require("./data/products");
+const orderList = require("./data/order");
 const {loadFilesSync} = require("@graphql-tools/load-files");
 const {makeExecutableSchema} = require("@graphql-tools/schema");
 
@@ -11,29 +13,8 @@ const schemaArray = loadFilesSync(path.join(__dirname,"**/*.graphql"));
 
 const schema = makeExecutableSchema({typeDefs: schemaArray});
 
-var shoapRoot = {
-    products : [
-  
-  ],
-  orders : [
-    
-    {
-        date : '2022-06-18',
-        totalPrice : 236.84,
-        items : [
-            {
-                product : {
-                    id: "Hat2",
-                    title: "Hat 2 product",
-                    description : "this product is hat 2",
-                    price : 45.99
-                },
-                quantity: 6
-            }
-        ]
-    }
-  ]
-}
+var shoapRoot = {products : productList,orders : orderList}
+
 const app = express();
 app.use("/graphql",createHandler({schema: schema,rootValue : shoapRoot}));
 app.get('/playground',expressPlayground({endpoint : '/graphql'}));
---
+++ data/order.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        date : '2022-06-18',
+        totalPrice : 236.84,
+        items : [
+            {
+                product : {
+                    id: "Hat2",
+                    title: "Hat 2 product",
+                    description : "this product is hat 2",
+                    price : 45.99
+                },
+                quantity: 6
+            }
+        ]
+    }
+]
